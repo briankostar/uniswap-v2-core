f99f9cfff14243ffd2ba5447535397dd04964f71
brian <brian.kostar@gmail.com>
2020-11-08 13:42:09 -0500

    updates

--- migrations/2_deployer.js
+++ migrations/2_deployer.js
@@ -1,32 +1,11 @@
 const { Wallet } = require('ethers')
 
-// const json = require('../build0/UniswapV2Factory.json')
-// const contract = require('@truffle/contract')
-// const UniswapV2Factory = contract(json)
-// UniswapV2Factory.setProvider(this.web3._provider)
-
-// const UniswapV2ERC20 = artifacts.require('UniswapV2ERC20')
-const UniswapV2Factory = artifacts.require('UniswapV2Factory')
-// const UniswapV2Factory = artifacts.require('../build0/UniswapV2Factory.json')
-// const UniswapV2Pair = artifacts.require('UniswapV2Pair')
-
+const json = require('../build0/UniswapV2Factory.json')
+const contract = require('@truffle/contract')
+const UniswapV2Factory = contract(json)
+UniswapV2Factory.setProvider(this.web3._provider)
 
 module.exports = async function(deployer, environment, accounts) {
   const ownerAddress = Wallet.fromMnemonic(process.env.OWNER_MNEMONIC).address
-  // deployer.deploy(UniswapV2ERC20)
-  // deployer.deploy(UniswapV2Pair)
   await deployer.deploy(UniswapV2Factory, ownerAddress, { from: ownerAddress, overwrite: false })
-
-  // await UniswapV2Factory.setFeeTo(ownerAddress)
-  // await deployer.deploy(UniswapV2Factory, ownerAddress)
 }
-
-// const json = require('@uniswap/v2-core/build/UniswapV2Factory.json')
-// const contract = require('@truffle/contract');
-// const UniswapV2Factory = contract(json);
-
-// UniswapV2Factory.setProvider(this.web3._provider);
-
-// module.exports = function(_deployer, network, accounts) {
-//   _deployer.deploy(UniswapV2Factory, accounts[0], {from: accounts[0]})
-// };
--- src/index.ts
+++ src/index.ts
@@ -15,21 +15,22 @@ import getRevertReason from 'eth-revert-reason'
 async function main() {
   console.log('running')
 //   const artifact = await import(`../build/contracts/UniswapV2Factory.json`)
-//   const IUniswapV2Pair = await import(`../build/contracts/IUniswapV2Pair.json`)
+  const IUniswapV2Pair = await import(`../build/contracts/IUniswapV2Pair.json`)
 
   const UniswapV2Factory = await import(`../build0/UniswapV2Factory.json`)
   const UniswapV2Pair = await import(`../build0/UniswapV2Pair.json`)
   const ERC20 = await import(`../build0/ERC20.json`)
   const ERC20b = await import(`../build0/ERC20b.json`)
 
-  const factoryAddress = '0xb36a2919D799D7519D805c4b59fB62FDd6a6A16e'
-  const erc20Address = '0xc5AD91b39bb4dB830C9B6706657258fA5BE7eF7f'
-  const erc20bAddress = '0x234B27f6E954eab12234Ae6BeA9726289E0D0e77' 
+  const factoryAddress = '0xbc3cBf3a409168154Ab4485DA6132Bc48b70F834'
+  const erc20Address = '0xf3a6f23E271F06181b0e44895ae916e264522379'
+  const erc20bAddress = '0x094999bE4D3AB3f81D2B0036418121A7a3486820' 
 
-//   const provider = new providers.JsonRpcProvider(process.env.PROVIDER_URL)
-  const provider = new providers.JsonRpcProvider('http://localhost:8545/')
+  const provider = new providers.JsonRpcProvider(process.env.PROVIDER_URL)
+//   const provider = new providers.JsonRpcProvider('http://localhost:8545/')
 
   const wallet = Wallet.fromMnemonic(process.env.OWNER_MNEMONIC!).connect(provider)
+  const owner = '0x8fBE57f7b1017e74da6B23819C85824a5969a232'
 //   const contractAddress = await getAddress(artifact, provider)
   const factoryContract = new Contract(factoryAddress, UniswapV2Factory.abi, wallet)
   const erc20Contract = new Contract(erc20Address, ERC20.abi, wallet)
@@ -47,8 +48,8 @@ async function main() {
   let allPairsLength = await factoryContract.allPairsLength()
   console.log('allPairsLength', allPairsLength.toString())
 
-//   let allPairs = await factoryContract.allPairs(0)
-//   console.log('allPairs(0)', allPairs)
+  let allPairs = await factoryContract.allPairs(0)
+  console.log('allPairs(0)', allPairs)
 
 //   let getPair = await factoryContract.getPair(
 //     erc20Address,
@@ -56,14 +57,18 @@ async function main() {
 //   )
 //   console.log('getPair', getPair)
 
-//   const pairAddress = '0xB03D823D59C3d5C57a2aB39547f8C43bcdC965B5'
-//   const pair = new Contract(pairAddress, JSON.stringify(IUniswapV2Pair.abi), provider).connect(wallet)
+  const pairAddress = '0x849cE15A40eeC7F91460196782331459B9579378'
+  const pair = new Contract(pairAddress, JSON.stringify(IUniswapV2Pair.abi), provider).connect(wallet)
 
-//   const token0 = await pair.token0()
-//   const token1 = await pair.token1()
-//   console.log('token0', token0)
-//   console.log('token1', token1)
-
+  const token0 = await pair.token0()
+  const token1 = await pair.token1()
+  console.log('token0', token0)
+  console.log('token1', token1)
+  const totalSupply = await pair.totalSupply()
+  console.log('totalSupply', totalSupply.toString())
+  const balanceOf = await pair.balanceOf(owner)
+  console.log('balanceOf', balanceOf.toString())
+  
   // have to get some WETH first
 //   let receipt = await wrappedContract.createPair(
 //     // '0xc778417E063141139Fce010982780140Aa0cD5Ab', // canonical weth
@@ -74,36 +79,36 @@ async function main() {
 //   )
 
 
-const uniBytecode = `0x${UniswapV2Factory.evm.bytecode.object}`
-const create2Address = getCreate2Address(factoryAddress, [erc20Address, erc20bAddress], uniBytecode)
-console.log('create2Address', create2Address);
+// const uniBytecode = `0x${UniswapV2Pair.evm.bytecode.object}`
+// const create2Address = getCreate2Address(factoryAddress, [erc20Address, erc20bAddress], uniBytecode)
+// console.log('create2Address', create2Address);
 
-  let receipt = await factoryContract.createPair(
-    erc20Address, erc20bAddress,
-    { gasLimit: 5000000 }
-  )
+//   let receipt = await factoryContract.createPair(
+//     erc20Address, erc20bAddress,
+//     { gasLimit: 5000000 }
+//   )
 
-  receipt
-    .wait()
-    .then(async (a: any) => {
-      console.log('receipt sucess!', a)
-      console.log('a', a.events[0].args);
+//   receipt
+//     .wait()
+//     .then(async (a: any) => {
+//       console.log('receipt sucess!', a)
+//       console.log('a', a.events[0].args);
       
 
-  let getPair = await factoryContract.getPair(
-    erc20Address,
-    erc20bAddress
-  )
-  console.log('getPair', getPair)
+//   let getPair = await factoryContract.getPair(
+//     erc20Address,
+//     erc20bAddress
+//   )
+//   console.log('getPair', getPair)
 
-    let allPairs = await factoryContract.allPairs(0)
-  console.log('allPairs(0)', allPairs)
-    })
-    .catch(async (err: any) => {
-      console.log('receipt err', err)
-      const reason = await getRevertReason(receipt.hash, 'development')
-      console.log('reason', reason)
-    })
+//     let allPairs = await factoryContract.allPairs(0)
+//   console.log('allPairs(0)', allPairs)
+//     })
+//     .catch(async (err: any) => {
+//       console.log('receipt err', err)
+//       const reason = await getRevertReason(receipt.hash, 'development')
+//       console.log('reason', reason)
+//     })
 
   // await new Promise((resolve, reject) => {
   //   provider.waitForTransaction(receipt.hash!, 5).then(receipt => {
